f1eccecb54213be0c6c2a92b7ff4608921d8d89d
dewynl <dewyn.liriano@gmail.com>
2024-05-22 22:42:21 -0400

    added celery tasks to process new researh records

---
+++ background_processes/process_new_research.py
@@ -0,0 +1,58 @@
+import logging
+
+from background_processes.nlp.extract_keywords import extract_keywords
+from background_processes.nlp.preprocess_text import preprocess_text
+from celery_setup import celery_app
+from db import get_db, Research, Status
+
+from transformers import pipeline
+
+@celery_app.task
+def process_new_research():
+    db_conn = next(get_db())
+
+    unprocessed_research = db_conn.query(Research).filter(Research.research_preprocess_status == Status.NOT_PROCESSED).first()
+    unprocessed_research.research_preprocess_status = Status.IN_PROGRESS
+    db_conn.commit()  # Updating status so another worker doesn't pick it up.
+
+    all_abstracts = ''
+    if unprocessed_research:
+        for article in unprocessed_research.articles:
+            processed_title, vectorized_title = preprocess_text(article.title)
+            processed_abstract, vectorized_abstract = preprocess_text(article.abstract)
+
+            article.cleaned_title = processed_title
+            article.title_vector = vectorized_title
+            article.cleaned_abstract = processed_abstract
+            article.abstract_vector = vectorized_abstract
+
+            all_abstracts += article.abstract + '\n\n'
+
+    keywords = extract_keywords(all_abstracts) if all_abstracts else []
+    unprocessed_research.keywords = keywords
+    unprocessed_research.research_preprocess_status = Status.DONE
+
+    db_conn.commit()
+
+@celery_app.task
+def summarize_research():
+    try:
+        db_conn = next(get_db())
+        research = db_conn.query(Research).filter(Research.summary_generation_status == Status.NOT_PROCESSED).first()
+
+        if research:
+            all_abstracts = "\n\n".join([article.abstract for article in research.articles])
+            summarizer = pipeline("summarization", model="t5-base", tokenizer="t5-base", framework="pt")
+            summary = summarizer(all_abstracts, max_length=500, min_length=50, do_sample=False)
+            research.summary = summary[0]['summary_text']
+            research.summary_generation_status = Status.DONE
+            db_conn.commit()
+        else:
+            logging.info("No research found for summarization.")
+    except Exception as e:
+        logging.error(f"Error occurred while summarizing research: {str(e)}")
+        db_conn.rollback()
+    finally:
+        db_conn.close()
+
+
--- celery_setup.py
+++ celery_setup.py
@@ -8,6 +8,7 @@ celery_broker_url = os.environ.get('CELERY_BROKER_URL', "redis://localhost:6379/
 celery_tasks_paths = [
     'data_sources.arxiv',
     'background_processes.create_research',
+    'background_processes.process_new_research'
 ]
 
 celery_app = Celery(__name__, broker=celery_broker_url, backend=celery_broker_url, include=celery_tasks_paths)
@@ -15,7 +16,11 @@ celery_app.conf.broker_connection_retry_on_startup = True
 
 celery_app.conf.beat_schedule = {
     'process_new_articles': {
-        'task': 'tasks.tasks.poll_newly_added_articles',
+        'task': 'background_processes.process_new_research.process_new_research',
         'schedule': timedelta(minutes=10),
     },
+    'summarize_new_research': {
+        'task': 'background_processes.process_new_research.summarize_research',
+        'schedule': timedelta(minutes=10),
+    }
 }
