e16bfafa7097e15d34eab9ff55af862fa549421f
dewynl <dewyn.liriano@gmail.com>
2024-05-23 00:14:47 -0400

    enabled a websocket endpoint and removed test endpoints

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import logging
 from typing import List
-
-from fastapi import FastAPI, Request, Depends, Body
+from fastapi import FastAPI, WebSocket, Request, Depends, Body
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session, subqueryload
+from sqlalchemy.orm import Session
 
 from data_sources.data_extractor import DataExtractor
 from db import create_all_tables, get_db, Article, Research
@@ -25,14 +24,13 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-async def root():
-    return "Hello World"
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        # Process the received data and send a response if needed
+        await websocket.send_text(f"Message received: {data}")
 
 
 @app.get("/search/{platform}")
@@ -53,6 +51,7 @@ async def search_articles(request: Request, db: Session = Depends(get_db)):
     search_term = request.query_params.get('query', '')
     found_articles = db.query(Article).filter(func.lower(Article.title).contains(search_term.lower()))
     return found_articles.all()
+
 
 @app.post("/extract/{platform}")
 def extract_data(_: Request, platform: str, article_ids: List[str] = Body(...)):
